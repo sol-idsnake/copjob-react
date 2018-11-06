ddfed981a6eb1fdfa7e10746b78e08cd78d67d96
Hendrik Wendt <h.wendt@gmx.com>
2018-11-06 18:36:16 -0500

    updated

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+module.exports = {
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080',
+};
 
-export default API_BASE_URL;
+// export default API_BASE_URL;

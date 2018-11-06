3867378494e138999d1477bdc48bf12f6f47e5a0
Hendrik Wendt <h.wendt@gmx.com>
2018-11-06 14:41:59 -0500

    prettified

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:8080/';
-
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
 
 export default API_BASE_URL;

c4a9dbe7bb6f081c81af85bc52ba8215902d6d39
Hendrik Wendt <h.wendt@gmx.com>
2018-11-18 16:38:55 -0500

    added async lazy loading

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import asyncComponent from './async/asyncComponent';
 import LandingPage from './landingpage';
-import Join from './join';
-import RegistrationForm from './registrationForm';
+// import Join from './join';
+// import RegistrationForm from './registrationForm';
+
+const AsyncJoin = asyncComponent(() => import('./join'));
+const AsyncRegistrationForm = asyncComponent(() => import('./registrationForm'));
 
 const App = () => (
   <Router>
     <div>
       <Route exact path="/" component={LandingPage} />
-      <Route exact path="/join" component={Join} />
-      <Route exact path="/register" component={RegistrationForm} />
+      <Route exact path="/join" component={AsyncJoin} />
+      <Route exact path="/register" component={AsyncRegistrationForm} />
     </div>
   </Router>
 );
